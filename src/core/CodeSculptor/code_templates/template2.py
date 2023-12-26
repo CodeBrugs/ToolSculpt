f8df5e90ab5d9f720f734cd04b32795d9d8b3140
Brugyto <153248786+Brugyto@users.noreply.github.com>
2023-12-26 11:30:20 -0800

    Update template2.py

--- src/core/CodeSculptor/code_templates/template2.py
+++ src/core/CodeSculptor/code_templates/template2.py
@@ -1,1 +1,80 @@
+import os
+import sys
+
+def cargar_contenido(ruta_archivo):
+    """
+    Carga el contenido de un archivo de texto.
+
+    Parameters:
+    - ruta_archivo (str): Ruta al archivo de entrada.
+
+    Returns:
+    - contenido (str): Contenido del archivo.
+    """
+    contenido = ""
+    try:
+        with open(ruta_archivo, 'r') as archivo:
+            contenido = archivo.read()
+    except FileNotFoundError:
+        print(f"Error: El archivo {ruta_archivo} no se encontró.")
+    except Exception as e:
+        print(f"Error inesperado al cargar el archivo: {e}")
+
+    return contenido
+
+def procesar_contenido(contenido):
+    """
+    Procesa el contenido del archivo.
+
+    Parameters:
+    - contenido (str): Contenido del archivo.
+
+    Returns:
+    - resultado (str): Resultado del procesamiento.
+    """
+    # Puedes personalizar la lógica de procesamiento aquí
+    resultado = contenido.upper()  # Ejemplo: Convertir a mayúsculas
+
+    return resultado
+
+def guardar_resultado(resultado, ruta_salida):
+    """
+    Guarda el resultado en un archivo.
+
+    Parameters:
+    - resultado (str): Resultado a guardar.
+    - ruta_salida (str): Ruta al archivo de salida.
+    """
+    try:
+        with open(ruta_salida, 'w') as archivo:
+            archivo.write(resultado)
+        print(f"Resultado guardado en {ruta_salida}")
+    except Exception as e:
+        print(f"Error inesperado al guardar el resultado: {e}")
+
+def ejecutar_programa(archivo_entrada, archivo_salida):
+    """
+    Punto de entrada del programa.
+
+    Parameters:
+    - archivo_entrada (str): Ruta al archivo de entrada.
+    - archivo_salida (str): Ruta al archivo de salida.
+    """
+    print("¡Hola, CodeSculptor!")
+
+    # Carga el contenido del archivo
+    contenido = cargar_contenido(archivo_entrada)
+
+    # Procesa el contenido y guarda el resultado
+    if contenido:
+        resultado = procesar_contenido(contenido)
+        guardar_resultado(resultado, archivo_salida)
+
+if __name__ == "__main__":
+    # Rutas a archivos de entrada y salida (personaliza según tu necesidad)
+    archivo_entrada = "texto_entrada.txt"
+    archivo_salida = "resultado_salida.txt"
+
+    # Ejecuta el programa
+    ejecutar_programa(archivo_entrada, archivo_salida)
 
